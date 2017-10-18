6d56837893f3f7b92fd26bef22dc9cacec9a0359
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2017-10-18 16:00:22 +0200

    ~ added javadoc and license info

--- slizaa-scanner-core/org.slizaa.scanner.core.contentdefinition/src/main/java/org/slizaa/scanner/core/contentdefinition/internal/DefaultFileBasedContentInfoResolver.java
+++ slizaa-scanner-core/org.slizaa.scanner.core.contentdefinition/src/main/java/org/slizaa/scanner/core/contentdefinition/internal/DefaultFileBasedContentInfoResolver.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (C) 2011-2017 Gerd Wuetherich (gerd@gerd-wuetherich.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Gerd Wuetherich (gerd@gerd-wuetherich.de) - initial API and implementation
+ ******************************************************************************/
 package org.slizaa.scanner.core.contentdefinition.internal;
 
 import java.io.File;
--- slizaa-scanner-core/org.slizaa.scanner.core.spi-api/src/main/java/org/slizaa/scanner/core/spi/parser/model/NodeFactory.java
+++ slizaa-scanner-core/org.slizaa.scanner.core.spi-api/src/main/java/org/slizaa/scanner/core/spi/parser/model/NodeFactory.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2011-2015 Slizaa project team.
+ * Copyright (C) 2011-2017 Gerd Wuetherich (gerd@gerd-wuetherich.de).
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *    Slizaa project team - initial API and implementation
+ *     Gerd Wuetherich (gerd@gerd-wuetherich.de) - initial API and implementation
  ******************************************************************************/
 package org.slizaa.scanner.core.spi.parser.model;
 
