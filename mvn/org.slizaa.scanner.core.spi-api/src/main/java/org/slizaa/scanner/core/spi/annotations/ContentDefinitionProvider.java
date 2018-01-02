58a334ddb8449c1ea940d8da0354302787a3e38f
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-01-02 15:32:21 +0100

    ~ renamed annotations

--- mvn/org.slizaa.scanner.core.spi-api/src/main/java/org/slizaa/scanner/core/spi/annotations/ContentDefinitionProvider.java
+++ mvn/org.slizaa.scanner.core.spi-api/src/main/java/org/slizaa/scanner/core/spi/annotations/ContentDefinitionProvider.java
@@ -23,6 +23,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface SlizaaContentDefinitionProvider {
+public @interface ContentDefinitionProvider {
 
 }
