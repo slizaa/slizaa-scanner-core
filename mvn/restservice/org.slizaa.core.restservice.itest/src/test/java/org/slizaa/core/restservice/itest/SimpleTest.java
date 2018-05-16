230da54b0ca9926b2073f43f40d30411fdfda8ea
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-05-16 15:49:48 +0200

    ~ support for jersey

---
+++ mvn/restservice/org.slizaa.core.restservice.itest/src/test/java/org/slizaa/core/restservice/itest/SimpleTest.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package org.slizaa.core.restservice.itest;
+
+import org.junit.Test;
+import org.osgi.framework.BundleException;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ *
+ */
+public class SimpleTest extends AbstractEclipseTest {
+
+  @Test
+  public void test() throws BundleException {
+
+    //
+    startAllBundles();
+
+    //
+  }
+}
