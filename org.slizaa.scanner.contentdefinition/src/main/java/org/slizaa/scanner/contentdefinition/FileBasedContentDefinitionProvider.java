3621cdfaca2fbe112d980855a3fe6d3ab9acc450
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2018-12-15 09:49:47 +0100

    ~ adjusted content provider

--- org.slizaa.scanner.contentdefinition/src/main/java/org/slizaa/scanner/contentdefinition/FileBasedContentDefinitionProvider.java
+++ org.slizaa.scanner.contentdefinition/src/main/java/org/slizaa/scanner/contentdefinition/FileBasedContentDefinitionProvider.java
@@ -13,7 +13,6 @@ package org.slizaa.scanner.contentdefinition;
 import java.io.File;
 import java.io.IOException;
 
-import org.ops4j.pax.url.mvn.MavenResolvers;
 import org.slizaa.scanner.spi.contentdefinition.AbstractContentDefinitionProvider;
 import org.slizaa.scanner.spi.contentdefinition.AnalyzeMode;
 import org.slizaa.scanner.spi.contentdefinition.IContentDefinition;
@@ -28,39 +27,27 @@ import org.slizaa.scanner.spi.contentdefinition.IContentDefinitionProvider;
  */
 @Deprecated
 public class FileBasedContentDefinitionProvider extends AbstractContentDefinitionProvider
-    implements IContentDefinitionProvider {
+		implements IContentDefinitionProvider {
 
-  @Override
-  public IContentDefinition createFileBasedContentDefinition(String contentName, String contentVersion,
-      File[] binaryPaths, File[] sourcePaths, AnalyzeMode analyzeMode) {
+	@Override
+	public IContentDefinition createFileBasedContentDefinition(String contentName, String contentVersion,
+			File[] binaryPaths, File[] sourcePaths, AnalyzeMode analyzeMode) {
 
-    //
-    return super.createFileBasedContentDefinition(contentName, contentVersion, binaryPaths, sourcePaths, analyzeMode);
-  }
+		//
+		return super.createFileBasedContentDefinition(contentName, contentVersion, binaryPaths, sourcePaths,
+				analyzeMode);
+	}
 
-  /**
-   * @param progressMonitor
-   */
-  protected void onInitializeProjectContent() {
+	@Override
+	protected void onInitializeProjectContent() {
+		//
+	}
 
-    try {
-      File resolvedFile = MavenResolvers.createMavenResolver(null, null).resolve("org.mapstruct", "mapstruct-processor",
-          null, "jar", "1.2.0.CR2");
-
-      this.createFileBasedContentDefinition("Module1", "1.2.3", new File[] { resolvedFile }, null,
-          AnalyzeMode.BINARIES_ONLY);
-
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected void onDisposeProjectContent() {
-    //
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onDisposeProjectContent() {
+		//
+	}
 }
--- org.slizaa.scanner.contentdefinition/src/main/java/org/slizaa/scanner/contentdefinition/MvnBasedContentDefinitionProvider.java
+++ org.slizaa.scanner.contentdefinition/src/main/java/org/slizaa/scanner/contentdefinition/MvnBasedContentDefinitionProvider.java
@@ -10,149 +10,83 @@
  ******************************************************************************/
 package org.slizaa.scanner.contentdefinition;
 
-import org.ops4j.pax.url.mvn.MavenResolvers;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slizaa.core.mvnresolver.MvnResolverServiceFactoryFactory;
+import org.slizaa.core.mvnresolver.api.IMvnCoordinate;
+import org.slizaa.core.mvnresolver.api.IMvnResolverService;
 import org.slizaa.scanner.spi.contentdefinition.AbstractContentDefinitionProvider;
 import org.slizaa.scanner.spi.contentdefinition.AnalyzeMode;
 import org.slizaa.scanner.spi.contentdefinition.IContentDefinitionProvider;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * <p>
- * Superclass for all implementations of {@link ITempDefinitionProvider}
  * </p>
  *
  * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
  */
 public class MvnBasedContentDefinitionProvider extends AbstractContentDefinitionProvider
-    implements IContentDefinitionProvider {
+		implements IContentDefinitionProvider {
 
-  /** - */
-  private List<MavenCoordinates> _mavenCoordinates;
+	/** - */
+	private List<String> _mavenCoordinates;
 
-  /**
-   * <p>
-   * Creates a new instance of type {@link MvnBasedContentDefinitionProvider}.
-   * </p>
-   */
-  public MvnBasedContentDefinitionProvider() {
-    _mavenCoordinates = new LinkedList<>();
-  }
+	/**
+	 * <p>
+	 * Creates a new instance of type {@link MvnBasedContentDefinitionProvider}.
+	 * </p>
+	 */
+	public MvnBasedContentDefinitionProvider() {
+		_mavenCoordinates = new LinkedList<>();
+	}
 
-  /**
-   * <p>
-   * </p>
-   *
-   * @param groupId
-   * @param artifactId
-   * @param version
-   */
-  public void addArtifact(String groupId, String artifactId, String version) {
-    _mavenCoordinates.add(new MavenCoordinates(groupId, artifactId, version));
-  }
+	/**
+	 * <p>
+	 * </p>
+	 *
+	 * @param groupId
+	 * @param artifactId
+	 * @param version
+	 */
+	@Deprecated
+	public void addArtifact(String groupId, String artifactId, String version) {
+		_mavenCoordinates
+				.add(String.format("%s:%s:%s", checkNotNull(groupId), checkNotNull(artifactId), checkNotNull(version)));
+	}
+	
+	public void addArtifact(String coordinate) {
+		_mavenCoordinates.add(coordinate);
+	}
 
-  /**
-   */
-  protected void onInitializeProjectContent() {
 
-    //
-    for (MavenCoordinates mavenCoordinates : _mavenCoordinates) {
+	/**
+	 */
+	protected void onInitializeProjectContent() {
 
-      String moduleName = mavenCoordinates.artifactId;
-      String moduleVersion = mavenCoordinates.version;
+		//
+		for (String coord : _mavenCoordinates) {
 
-      try {
-        File resolvedFile = MavenResolvers.createMavenResolver(null, null).resolve(mavenCoordinates.getGroupId(),
-            mavenCoordinates.getArtifactId(), null, "jar", mavenCoordinates.getVersion());
+			// TODO
+			IMvnResolverService resolverService = MvnResolverServiceFactoryFactory.createNewResolverServiceFactory()
+					.newMvnResolverService().withDefaultRemoteRepository().create();
 
-        this.createFileBasedContentDefinition(moduleName, mavenCoordinates.version, new File[] { resolvedFile }, null,
-            AnalyzeMode.BINARIES_ONLY);
+			IMvnCoordinate mvnCoordinate = resolverService.parseCoordinate(coord);
+			File resolvedFile = resolverService.resolveArtifact(coord);
 
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
-    }
-  }
+			this.createFileBasedContentDefinition(mvnCoordinate.getArtifactId(), mvnCoordinate.getVersion(),
+					new File[] { resolvedFile }, null, AnalyzeMode.BINARIES_ONLY);
+		}
+	}
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected void onDisposeProjectContent() {
-    //
-  }
-
-  /**
-   * <p>
-   * </p>
-   *
-   * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
-   */
-  static class MavenCoordinates {
-
-    private String groupId;
-
-    private String artifactId;
-
-    private String version;
-
-    public MavenCoordinates(String groupId, String artifactId, String version) {
-      this.groupId = groupId;
-      this.artifactId = artifactId;
-      this.version = version;
-    }
-
-    public String getGroupId() {
-      return groupId;
-    }
-
-    public String getArtifactId() {
-      return artifactId;
-    }
-
-    public String getVersion() {
-      return version;
-    }
-
-    @Override
-    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((artifactId == null) ? 0 : artifactId.hashCode());
-      result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
-      result = prime * result + ((version == null) ? 0 : version.hashCode());
-      return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj)
-        return true;
-      if (obj == null)
-        return false;
-      if (getClass() != obj.getClass())
-        return false;
-      MavenCoordinates other = (MavenCoordinates) obj;
-      if (artifactId == null) {
-        if (other.artifactId != null)
-          return false;
-      } else if (!artifactId.equals(other.artifactId))
-        return false;
-      if (groupId == null) {
-        if (other.groupId != null)
-          return false;
-      } else if (!groupId.equals(other.groupId))
-        return false;
-      if (version == null) {
-        if (other.version != null)
-          return false;
-      } else if (!version.equals(other.version))
-        return false;
-      return true;
-    }
-  }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void onDisposeProjectContent() {
+		//
+	}
 }
